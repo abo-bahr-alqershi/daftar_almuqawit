31f747fd555fcede9bd82606594a80892e19cd5c
Abdullah Alqershi <abdullah-alqershi@gmail.com>
2025-11-02 22:07:22 +0300

    update

--- lib/core/services/sync/sync_manager.dart
+++ lib/core/services/sync/sync_manager.dart
@@ -1,1 +1,23 @@
-class SyncManager {}
+import 'package:get_it/get_it.dart';
+
+import 'sync_service.dart';
+import 'offline_queue.dart';
+import 'conflict_resolver.dart';
+
+class SyncManager {
+  final _sl = GetIt.instance;
+
+  SyncService get _service => _sl<SyncService>();
+  OfflineQueue get _offlineQueue => _sl<OfflineQueue>();
+  ConflictResolver get _resolver => _sl<ConflictResolver>();
+
+  Future<void> syncNow() => _service.syncOnce();
+  void startAuto() => _service.startAutoSync();
+  Future<void> stopAuto() => _service.disposeAutoSync();
+  Future<void> resolveConflicts() => _resolver.resolveAll();
+
+  Future<void> enqueue(String entity, String operation, Map<String, Object?> payload) =>
+      _offlineQueue.enqueue(entity, operation, payload);
+
+  Future<int> pendingCount() => _offlineQueue.pendingCount();
+}
